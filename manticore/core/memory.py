89215a0059a7f1f1db8a52cfc3728785b867d788
yan <rottled@gmail.com>
2018-07-10 18:30:50 -0400

    make mmapFile depend on super.mmapFile

--- manticore/core/memory.py
+++ manticore/core/memory.py
@@ -1105,56 +1105,12 @@ class LazySMemory(SMemory):
         # self.bigarray = ArrayMap(0, 2**32 - 1, 'rwx', 32, name='bigarray')
         self.bigarray = constraints.new_array(index_bits=self.memory_bit_size)
 
-    def mmap(self, addr, size, perms, name=None, **kwargs):
-        assert isinstance(addr, (int, long))
-        map = AnonMap(addr, size, perms, name)
-        self._add(map)
-        return addr
 
     def mmapFile(self, addr, size, perms, filename, offset=0):
-        '''
-        Creates a new file mapping in the memory address space.
-
-        :param addr: the starting address (took as hint). If C{addr} is C{0} the first big enough
-                     chunk of memory will be selected as starting address.
-        :param size: the contents of a file mapping are initialized using C{size} bytes starting
-                     at offset C{offset} in the file C{filename}.
-        :param perms: the access permissions to this memory.
-        :param filename: the pathname to the file to map.
-        :param offset: the contents of a file mapping are initialized using C{size} bytes starting
-                      at offset C{offset} in the file C{filename}.
-        :return: the starting address where the file was mapped.
-        :rtype: int
-        :raises error:
-                   - 'Address shall be concrete' if C{addr} is not an integer number.
-                   - 'Address too big' if C{addr} goes beyond the limit of the memory.
-                   - 'Map already used' if the piece of memory starting in C{addr} and with length C{size} isn't free.
-        '''
-        # If addr is NULL, the system determines where to allocate the region.
-        assert addr is None or isinstance(addr, (int, long)), 'Address shall be concrete'
-        assert addr < self.memory_size, 'Address too big'
-        assert size > 0
-
-        map = AnonMap(addr, size, perms)
-        self._add(map)
-
-        # address is rounded down to the nearest multiple of the allocation granularity
-        if addr is not None:
-            addr = self._floor(addr)
-
-        # size value is rounded up to the next page boundary
-        size = self._ceil(size)
-
-        with open(filename, 'r') as f:
-            fdata = f.read()
-
-        towrite = min(size, len(fdata[offset:]))
-
-        for i in xrange(towrite):
-            self.bigarray[addr+i:addr+i+1] = fdata[offset+i]
-
-        logger.debug('New file-memory map @%x size:%x', addr, size)
-
+        addr = super(LazySMemory, self).mmapFile(addr, size, perms, filename, offset)
+        m = self.map_containing(addr)
+        for offset in range(m.start, m.end):
+            self.bigarray[offset] = m[offset]
         return addr
 
     def _deref_can_succeed(self, map, address, size):
